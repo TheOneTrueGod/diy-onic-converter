80fc8a5e4b353e5571fffb7bb666917374b0f79e
TheOneTrueGod <TheOneTrueGod180@hotmail.com>
2024-10-24 17:30:29 -0700

    Filling out the stub

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
+# To execute the function
+Copy everything in public/diy-onic-converter.js and paste it into the console of any web page.
+
+Then call `diyOnicConverter(#QUERY_SELECTOR)` and it will transform the page.
+Might I recommend `diyOnicConverter('body')` for the discerning code reviewer?
+
+# Notes from Jeremy
+Hi, and welcome to my interview answer for diy-onic-converter.
+
+The approach I used was to navigate through the document tree recursively.
+We're not at a particularly high risk of running out of memory from the recursive approach, and an iterative approach seemed more complicated for little gain.
+Once I found the text nodes, I used a tokenizer based approach to split each text node up into words, and then replaced each word with a new DOM element.
+I tried to keep the DOM as flat as I could -- this will add at most one additonal Element and one additional TextNode of depth to the DOM.
+
+This approach allows the structure of the document to remain in place (anchor tags, <strong> tags, etc), and gives us some pretty strong flexibility when it comes to identifying words and where / how to place the bolding.
+
+I ignored the suggestion to start with the basic requirements.  I had the DOM Node structure explained to me many years ago, and I wanted to take this opportunity to learn it first hand, and once I was doing that it was easier to just go for the advanced version.
+I enjoyed re-learning the tokenizer approaches I was taught in my university days!
+
+# Testing
+
+I tested it out on this wikipedia page [Fuso class battleship](https://en.wikipedia.org/wiki/Fus%C5%8D-class_battleship) and found that it messed with the layout of images a bit.
+
+
 # diy-onic-converter
 Coding challenge to make your own (DIY) “bionic reading converter.”
 
--- public/diy-onic-converter.js
+++ public/diy-onic-converter.js
@@ -1,12 +1,108 @@
 /**
  * Implement your converter function here.
  */
+
+const CHARS_TO_BOLD = 2
+
+// enum values
+const ELEMENT_NODE = 1
+const TEXT_NODE = 3
+
+// Won't work for other languages :(
+function isLetter(str) {
+  return str.length === 1 && str.match(/[A-Za-z]/i);
+}
+
+// We use isLetter to find the start of a word, and isLetterOrNumber to find the end of a word, so that we include acronyms or l33t speak
+function isLetterOrNumber(str) {
+  return str.length === 1 && str.match(/[A-Za-z.0-9]/i);
+}
+// Also need to deal with periods and numbers
+
+const convertWord = (word) => {
+  // If you wanted to change the number of characters to bold, this is the place to do it
+  const wrapper = document.createElement('span')
+  const boldLength = Math.min(CHARS_TO_BOLD, word.length)
+  const strongCharacters = word.slice(0, boldLength)
+  const weakCharacters = word.slice(boldLength, word.length)
+
+  const strongPart = document.createElement('strong')
+  strongPart.appendChild(document.createTextNode(strongCharacters))
+
+  const weakPart = document.createTextNode(weakCharacters)
+
+  wrapper.appendChild(strongPart)
+  wrapper.appendChild(weakPart)
+
+  return wrapper
+}
+
+// Use a lexer / tokenizer approach to iterate through the string so we only replace actual words
+const convertNode = (node) => {
+  const newNode = document.createElement('span')
+  const nodeText = node.nodeValue
+  // iterate through characters
+  let wordStart = undefined
+  let lastWordEnd = 0
+  for (let i = 0; i < nodeText.length; i++) {
+    if (wordStart === undefined && isLetter(nodeText[i])) {
+      wordStart = i
+      if (lastWordEnd !== i) {
+        // Up to now, there were no words.  Add the text as unemphasized
+        newNode.appendChild(document.createTextNode(nodeText.slice(lastWordEnd, wordStart)))
+      }
+    }
+    if (wordStart !== undefined && !isLetterOrNumber(nodeText[i])) {
+      const word = nodeText.slice(wordStart, i)
+      // We found a word!  Convert it and add it
+      newNode.appendChild(convertWord(word))
+
+      // Update our position marker
+      lastWordEnd = i
+      wordStart = undefined
+    }
+  }
+
+  if (wordStart === undefined) {
+    // We didn't find the start of a word.  Just add the rest unemphasized.
+    newNode.appendChild(document.createTextNode(nodeText.slice(lastWordEnd)))
+  } else {
+    // We found the start of a word, but not the end.  Add the remainder as a word
+    newNode.appendChild(convertWord(nodeText.slice(wordStart)))
+  }
+  node.replaceWith(newNode)
+}
+
+const recursivelyConvert = (container) => {
+  if (!container.childNodes) {
+    return // done
+  }
+
+  container.childNodes.forEach((node) => {
+    switch (node.nodeType) {
+      case TEXT_NODE:
+        convertNode(node)
+        break
+      case ELEMENT_NODE:
+        recursivelyConvert(node)
+        break
+      default:
+        // As far as I know (and through my testing), the above should be sufficient
+        break
+    }
+  })
+}
+
 const diyOnicConverter = (textContentContainerSelector) => {
   const container = document.querySelector(textContentContainerSelector);
-  console.log("Performing bionic reading conversion on:", container);
-
+  recursivelyConvert(container)
   /* Etc. etc. etc. your code etc. */
 };
 
 // Allow global access so that this can be executed from the console.
 window.diyOnicConverter = diyOnicConverter;
+
+// Uncomment me for faster / easier development
+/*window.onload = () => {
+  diyOnicConverter(".convertMe")
+}*/
